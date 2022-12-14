2d4a7f6950389ca5351cf5affa6e25b5c25c9428
abdulkader-safi <safi.abdulkader@gmail.com>
2022-12-14 21:17:18 +0200

    Setup Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { Home, Store, About } from './pages'
+import { Navbar } from './components'
 
 const App = () => {
   return (
-    <Container className='mb-4'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </>
+
   )
 }
 
